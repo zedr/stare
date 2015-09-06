6c1d75934de68d2737ac3623408b2eab319d9463
Rigel Di Scala <rigel.discala@propylon.com>
2015-09-06 21:57:24 +0100

    Delint sources

--- src/stare/str.c
+++ src/stare/str.c
@@ -4,6 +4,7 @@
 struct str_arr *create_str_arr(char *first)
 {
 	struct str_arr *ptr = (struct str_arr *) malloc(sizeof(*ptr));
+
 	if (first != NULL) {
 		ptr->strs = malloc(sizeof(ptr->strs));
 		ptr->strs[0] = first;
@@ -11,5 +12,6 @@ struct str_arr *create_str_arr(char *first)
 	} else {
 		ptr->len = 0;
 	}
+
 	return ptr;
 }
--- src/tests/str_tests.c
+++ src/tests/str_tests.c
@@ -7,8 +7,10 @@ MU_TEST(test_create_arr_of_str_from_scratch)
 {
 	struct str_arr *ptr = create_str_arr("test");
 	char *str = (ptr->strs)[0];
+
 	mu_assert(ptr->len == 1, "length of sequence is not 0");
 	mu_assert(strcmp("test", str) == 0, "first element is not equal to 1");
+
 	free(ptr);
 }
 
